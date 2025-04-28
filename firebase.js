d1b8ec0749e80707a7567b14ff864604dac415d9
CabriBarber <cabribarber32@gmail.com>
2025-04-28 19:07:16 -0300

    nuevo

--- firebase.js
+++ firebase.js
@@ -1,5 +1,4 @@
-// Importar SDKs tradicionales para Firebase
-// Esto debe estar en tu HTML, pero simulo aquí cómo debería ser usado
+// Inicializar Firebase usando el método clásico
 var firebaseConfig = {
   apiKey: "AIzaSyBlJvTey3YXLMr0lVyUGzlcYhiFGzAFWN0",
   authDomain: "cabriweb.firebaseapp.com",
@@ -8,7 +7,5 @@ var firebaseConfig = {
   messagingSenderId: "1083893391061",
   appId: "1:1083893391061:web:14c6fd57798d3705f90c5c"
 };
-
-// Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
